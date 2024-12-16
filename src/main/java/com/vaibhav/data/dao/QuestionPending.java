8ce7af2117a019943b8eec2b6f829db9495e882a
Vaibhav <vaibhav.ppp78@gmail.com>
2024-12-16 18:38:47 +0530

    Admin Portal

--- src/main/java/com/vaibhav/data/dao/QuestionPending.java
+++ src/main/java/com/vaibhav/data/dao/QuestionPending.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Document(collection = "questions")
+@Document(collection = "questionsPending")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
